c78e3d334c2ce4ded23425983ab09d43cc610568
Ealanrian <493440+Ealanrian@users.noreply.github.com>
2020-01-15 10:55:14 +0100

    Fixed test

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,21 +6,21 @@ namespace GradeBook.Tests
     public class BookTests
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAnAverageGrade()
         {
            
             // arrange
             Book book = new Book("");
 
-            book.AddGrade(80.1);
+            book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
             //act
             var result = book.GetStatistics();
             //assert
-            Assert.Equal(85.6, result.Average);
-            Assert.Equal(90.5, result.High);
-            Assert.Equal(77.3, result.Low);
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
         }
     }
 }
