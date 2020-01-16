0332bbb75cc13ac6dba0bacbc4480a2e99ef6cc6
Ealanrian <493440+Ealanrian@users.noreply.github.com>
2020-01-16 11:23:07 +0100

    Interface

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -10,7 +10,7 @@ namespace GradeBook.Tests
         {
            
             // arrange
-            Book book = new Book("");
+            InMemoryBook book = new InMemoryBook("");
 
             book.AddGrade(89.1);
             book.AddGrade(90.5);
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -59,9 +59,9 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void GetBookSetName(ref Book book, string name)
+        private void GetBookSetName(ref InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
         [Fact]
@@ -77,9 +77,9 @@ namespace GradeBook.Tests
             Assert.Equal("Book 1", book1.Name);
         }
 
-        private void GetBookSetName(Book book, string name)
+        private void GetBookSetName(InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
         [Fact]
@@ -95,7 +95,7 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(Book book, string name)
+        private void SetName(InMemoryBook book, string name)
         {
             book.Name = name;
         }
@@ -141,9 +141,9 @@ namespace GradeBook.Tests
             Assert.Same(book1, book2);
         }
 
-        private Book GetBook(string name)
+        private InMemoryBook GetBook(string name)
         {
-            return new Book(name);
+            return new InMemoryBook(name);
         }
     }
 }
